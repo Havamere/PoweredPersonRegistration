04659b489b9162e4b1260652d7fdf0c8db459055
Kyle R <kyle.rinehart83@gmail.com>
2016-09-07 17:04:00 -0400

    Changed pictures to ones that work with face ++, server is running, first page done, needs to look better, second page needs js and ajax working

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,3 +1,26 @@
+var matches = [
+	{name: 'Agent Hill', url: './images/Agent_Hill.jpg', id: '', powers: ['Aim Accuracy', 'Espionage']},
+	{name: 'Black Panther', url: './images/Black_Panter.jpg', id: '', powers: ['Agility', 'Martial Arts']},
+	{name: 'Black Widow', url: './images/Black_Widow.jpg', id: '', powers: ['Agility', 'Aim Accuracy', 'Espionage', 'Martial Arts']},
+	{name: 'Captain America', url: './images/Captain_America.jpg', id: '', powers: ['']},
+	{name: 'Daredevil', url: './images/Daredevil.jpg', id: '', powers: ['']}},
+	{name: 'Doctor Strange', url: './images/Doctor_Strange.jpg', id: '', powers: ['']}},
+	{name: 'Hawkeye', url: './images/Hawkeye.jpg', id: '', powers: ['']}},
+	{name: 'Jean Grey', url: './images/Jean_Grey.jpg', id: '', powers: ['']}},
+	{name: 'Jessica Jones', url: './images/Jessica_Jones.jpg', id: '', powers: ['']}},
+	{name: 'Loki', url: './images/loki.jpg', id: '', powers: ['']}},
+	{name: 'Powerman', url: './imagesPowerman.jpg', id: '', powers: ['']}},
+	{name: 'Professor Xavier', url: './images/Professor_Xavier.jpg', id: '', powers: ['']}},
+	{name: 'Punisher', url: './images/Punisher.jpg', id: '', powers: ['']}},
+	{name: 'Quicksilver', url: './images/Quicksilver.jpg', id: '', powers: ['']}},
+	{name: 'Scarlet Witch', url: './images/scarlet_witch.jpg', id: '', powers: ['']}},
+	{name: 'Spiderman', url: './images/Spiderman.jpg', id: '', powers: ['']}},
+	{name: 'Starlord', url: './images/Star_Lord.jpg', id: '', powers: ['']}},
+	{name: 'Storm', url: './images/Storm.jpg', id: '', powers: ['']}},
+	{name: 'Thor', url: './images/Thor.jpg', id: '', powers: ['']}},
+	{name: 'Wolverine', url: './images/Wolverine.png', id: '', powers: ['']}}
+]
+
 //||General functions for index/sign-in page||\\
 
 //creates firebase configuration for data management
@@ -34,6 +57,11 @@ $('#signUp').click(function(){
 	  var userEmail = result.user.email;
 
 	  var userPhoto = result.user.photoURL;
+
+	  var userLocation = result.user.location;
+
+	  //Creates a local storage spage for the user's info for the page's duration on the user's browser
+	  sessionStorage.setItem('user', JSON.stringify({user: userName, photo: userPhoto, location: userLocation}));
 
 	  	$.ajax({
 		  method: "POST",
@@ -77,6 +105,11 @@ $('#signIn').click(function(){
 
 	  var userPhoto = result.user.photoURL;
 
+	  var userLocation = result.user.location;
+
+	  //Creates a local storage spage for the user's info for the page's duration on the user's browser
+	  sessionStorage.setItem('user', JSON.stringify({user: userName, photo: userPhoto, location: userLocation}));
+
 	  	$.ajax({
 		  method: "POST",
 		  url: "/signIn",
@@ -103,7 +136,19 @@ $('#signIn').click(function(){
 
 });
 
+//||JavaScript for Scan page||\\
+$(document).ready(function() {
+	var user = JSON.parse(sessionStorage.getItem('user'));
 
+	//for (var i = 0; i < array.length; i++) {}
+	$('.profilePics').append("<img id='picture' src="+user.photo+" draggable='true' ondragstart='drag(event)'' width='200px' height='200px'>");
+
+	$('#submit').on('click', function() {
+		var userProfile = 
+	});
+});
+
+//Sign-out Button
 firebase.auth().signOut().then(function() {
   // Sign-out successful.
 }, function(error) {
