9dae87b999b8064a0711726df4f334b2b86e1412
Kyle R <kyle.rinehart83@gmail.com>
2016-09-10 13:13:35 -0400

    attempting fix of log-in 9

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -43,6 +43,77 @@ provider.addScope('user_location');
 
 
 $('#signUp').click(function(){
+	console.log("button clicked");
+	//sign-in by redirect
+	firebase.auth().getRedirectResult().then(function(result) {
+		console.log("attempting log in")
+	  if (result.credential) {
+	    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+	    var token = result.credential.accessToken;
+	    // ...
+	  }
+	  // The signed-in user info.
+	  var user = result.user;
+	  console.log(user);
+
+	  var userName = result.user.displayName;
+	  //console.log(userName);
+
+	  var userEmail = result.user.email;
+
+	  var userPhoto = result.user.photoURL;
+
+	  var userLocation = result.user.location;
+
+	  //Creates a local storage spage for the user's info for the page's duration on the user's browser
+	  sessionStorage.setItem('user', JSON.stringify({user: userName, photo: userPhoto, location: userLocation}));
+	  
+	  	$.ajax({
+		  method: "POST",
+		  url: "/signUp",
+		  data: {user: userName, email: userEmail, profilePic: userPhoto}
+		})
+		  .done(function( data ) {
+		  	if (data.error) {
+		  		alert(data.error);
+		  	}
+		  	alert(data.msg);
+		  	window.location = data.url;
+		  });
+
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+
+	// firebase.auth().signInWithPopup(provider).then(function(result) {
+	//   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+	//   var token = result.credential.accessToken;
+	//   // The signed-in user info.
+	//   var user = result.user;
+
+
+	//   // ...
+	// }).catch(function(error) {
+	//   // Handle Errors here.
+	//   var errorCode = error.code;
+	//   var errorMessage = error.message;
+	//   // The email of the user's account used.
+	//   var email = error.email;
+	//   // The firebase.auth.AuthCredential type that was used.
+	//   var credential = error.credential;
+	//   // ...
+	// });
+});
+
+$('#signIn').click(function(){
+	console.log("button clicked");
 	//sign-in by redirect
 	firebase.auth().getRedirectResult().then(function(result) {
 	  if (result.credential) {
@@ -68,7 +139,7 @@ $('#signUp').click(function(){
 	  
 	  	$.ajax({
 		  method: "POST",
-		  url: "/signUp",
+		  url: "/signIn",
 		  data: {user: userName, email: userEmail, profilePic: userPhoto}
 		})
 		  .done(function( data ) {
@@ -108,52 +179,6 @@ $('#signUp').click(function(){
 	//   var credential = error.credential;
 	//   // ...
 	// });
-});
-
-$('#signIn').click(function(){
-
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  console.log(user);
-
-	  var userName = result.user.displayName;
-	  //console.log(userName);
-
-	  var userEmail = result.user.email;
-
-	  var userPhoto = result.user.photoURL;
-
-	  var userLocation = result.user.location;
-
-	  //Creates a local storage spage for the user's info for the page's duration on the user's browser
-	  sessionStorage.setItem('user', JSON.stringify({user: userName, photo: userPhoto, location: userLocation}));
-
-	  	$.ajax({
-		  method: "POST",
-		  url: "/signIn",
-		  data: {user: userName, email: userEmail, profilePic: userPhoto}
-		})
-		  .done(function( data ) {
-		  	if (data.error) {
-		  		alert(data.error);
-		  	}
-		  	alert(data.msg);
-		  	window.location = data.url;
-		  });
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});
 
 });
 
