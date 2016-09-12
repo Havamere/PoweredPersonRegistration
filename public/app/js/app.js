6b9ee5e7995c1e98101f439f987d36e43ee5c4a2
Kyle R <kyle.rinehart83@gmail.com>
2016-09-12 18:57:38 -0400

    attempting fix of facebook sdk login 2

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -218,15 +218,6 @@ window.fbAsyncInit = function() {
     });
   }
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '{your-app-id}',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.5' // use graph api version 2.5
-  });
-
   // Now that we've initialized the JavaScript SDK, we call 
   // FB.getLoginStatus().  This function gets the state of the
   // person visiting this page and can return one of three states to
@@ -239,20 +230,12 @@ window.fbAsyncInit = function() {
   //
   // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
+	  FB.getLoginStatus(function(response) {
+	    statusChangeCallback(response);
+	  });
 
   };
 
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
