565bfaddb8578e737af9d99330163acdb797723f
Kyle R <kyle.rinehart83@gmail.com>
2016-09-15 14:14:28 -0400

    changing api key

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = { 
 	dbURI: "heroku_p3lkwd22:3s08qo57uvane1fe15ll64h2k1@ds029446.mlab.com:29446/heroku_p3lkwd22",
-	collections: 'poweredIndex'
+	collections: 'poweredIndex',
+	mapKey: "AIzaSyB76GN2pXdeqIyw_y61bvdS5NuR8qR2wXw",
 }
