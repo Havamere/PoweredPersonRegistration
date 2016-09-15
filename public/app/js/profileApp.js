5bf59d2f53bf454cc06bbe95a99b990e560911d0
Kyle R <kyle.rinehart83@gmail.com>
2016-09-15 11:35:26 -0400

    Power's now correspond to map search values

--- public/app/js/profileApp.js
+++ public/app/js/profileApp.js
@@ -21,15 +21,80 @@ var matches = [
 	{name: 'Wolverine', url: './images/Wolverine.png', powers: ['Enhanced Senses', 'Martial Arts', 'Regeneration']}
 ];
 
+var mapKey = "AIzaSyB76GN2pXdeqIyw_y61bvdS5NuR8qR2wXw"
+
 $(document).ready(function() {
 	var user = JSON.parse(sessionStorage.getItem('user'));
 
 	console.log(user);
 
-		$('#profilePic').append("<img class='pull-right' src="+user.user.profilePic+" width='100px' height='100px'>");
-		$('#userName').html(user.user.user);
-		for (var i = 0; i < user.user.powers.length; i++) {
-			$('#powers').append("<button class='btn btn-danger' value="+user.user.powers[i]+">"+user.user.powers[i]+"</button>");
-		};
+	var powers = user.user.powers;
+	var places = [];
+	//Aim Accuracy, Agility, Endurance, Enhanced Senses, Espionage, Flight, Invulnerability, Luck, Magic, Martial Arts, Mind Control, Regeneration, Speed, Stamina, Strategist, Strength, Telekenisis, Telepathy, Weather Control
+	for (var i = 0; i < powers.length; i++) {
+		if (powers[i] == "Aim Accuracy") {
+			places.push('gun range');
+		}
+		if (powers[i] == "Agility") {
+			places.push('gymnastics');
+		}
+		if (powers[i] == "Endurance") {
+			places.push('cross fit');
+		}
+		if (powers[i] == "Enhanced Senses") {
+			places.push('restaurants');
+		}
+		if (powers[i] == "Espionage") {
+			places.push('acting schools');
+		}
+		if (powers[i] == "Flight") {
+			places.push('sky diving');
+		}
+		if (powers[i] == "Invulnerability") {
+			places.push('boxing');
+		}
+		if (powers[i] == "Luck") {
+			places.push('gambling');
+		}
+		if (powers[i] == "Magic") {
+			places.push('magic shop');
+		}
+		if (powers[i] == "Martial Arts") {
+			places.push('MMA');
+		}
+		if (powers[i] == "Mind Control") {
+			places.push('magic show');
+		}
+		if (powers[i] == "Regeneration") {
+			places.push('blood bank');
+		}
+		if (powers[i] == "Speed") {
+			places.push('track and field');
+		}
+		if (powers[i] == "Stamina") {
+			places.push('track and field');
+		}
+		if (powers[i] == "Strategist") {
+			places.push('board games');
+		}
+		if (powers[i] == "Strength") {
+			places.push('gym');
+		}
+		if (powers[i] == "Telekenisis") {
+			places.push('magic show');
+		}
+		if (powers[i] == "Telepathy") {
+			places.push('psychic');
+		}
+		if (powers[i] == "Weather Control") {
+			places.push('weather station');
+		}
+	}
+
+	$('#profilePic').append("<img class='pull-right' src="+user.user.profilePic+" width='100px' height='100px'>");
+	$('#userName').html(user.user.user);
+	for (var i = 0; i < powers.length; i++) {
+		$('#powers').append("<button class='btn btn-danger' value="+places[i]+">"+powers[i]+"</button>");
+	};
 		
 });
