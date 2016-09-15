2deea0187a76a2f230126994ffb18db82f2a5e26
Kyle R <kyle.rinehart83@gmail.com>
2016-09-15 16:51:41 -0400

    changing location of script

--- public/app/js/profileApp.js
+++ public/app/js/profileApp.js
@@ -96,11 +96,58 @@ $(document).ready(function() {
 	};
 
 	var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-		
+		function initMap() {
+	    	map = new google.maps.Map(document.getElementById('map'), {
+	    	center: {lat: -34.397, lng: 150.644},
+	    	zoom: 8
+	    });
+	}
+
+	function initAutocomplete() {
+	    // Create the autocomplete object, restricting the search to geographical
+	    // location types.
+	    autocomplete = new google.maps.places.Autocomplete(
+	        /** @type {!HTMLInputElement} */
+	        (document.getElementById('autocomplete')), { types: ['geocode'] });
+
+	    // When the user selects an address from the dropdown, populate the address
+	    // fields in the form.
+	    autocomplete.addListener('place_changed', fillInAddress);
+	}	
+	
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	function geolocate() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(function(position) {
+	            var geolocation = {
+	                lat: position.coords.latitude,
+	                lng: position.coords.longitude
+	            };
+	            var circle = new google.maps.Circle({
+	                center: geolocation,
+	                radius: position.coords.accuracy
+	            });
+	            autocomplete.setBounds(circle.getBounds());
+	        });
+	    }
+	}
+
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	function geolocate() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(function(position) {
+	            var geolocation = {
+	                lat: position.coords.latitude,
+	                lng: position.coords.longitude
+	            };
+	            var circle = new google.maps.Circle({
+	                center: geolocation,
+	                radius: position.coords.accuracy
+	            });
+	            autocomplete.setBounds(circle.getBounds());
+	        });
+	    }
+	}
 });
