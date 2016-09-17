ddfe6403f45459e407eebe1a9989dd4ad159e8a5
Kyle R <kyle.rinehart83@gmail.com>
2016-09-17 01:27:23 -0400

    testing placement of all js inside doc on ready function 2

--- public/app/js/profileApp.js
+++ public/app/js/profileApp.js
@@ -32,238 +32,238 @@ var markerArr = [];
 
 $(document).ready(function() {
 
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 28.538, lng: -81.379},
-      zoom: 6
-    });
-    var infoWindow = new google.maps.InfoWindow({map: map});
+	function initMap() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+	      center: {lat: 28.538, lng: -81.379},
+	      zoom: 6
+	    });
+	    var infoWindow = new google.maps.InfoWindow({map: map});
 
-    // // Try HTML5 geolocation.
-    // if (navigator.geolocation) {
-    //   navigator.geolocation.getCurrentPosition(function(position) {
-    //     var pos = {
-    //       lat: position.coords.latitude,
-    //       lng: position.coords.longitude
-    //     };
+	    // Try HTML5 geolocation.
+	    if (navigator.geolocation) {
+	      navigator.geolocation.getCurrentPosition(function(position) {
+	        var pos = {
+	          lat: position.coords.latitude,
+	          lng: position.coords.longitude
+	        };
 
-    //     infoWindow.setPosition(pos);
-    //     infoWindow.setContent('Location found.');
-    //     map.setCenter(pos);
-    //   }, function() {
-    //     handleLocationError(true, infoWindow, map.getCenter());
-    //   });
-    // } else {
-    //   // Browser doesn't support Geolocation
-    //   handleLocationError(false, infoWindow, map.getCenter());
-    // }
-}
+	        infoWindow.setPosition(pos);
+	        infoWindow.setContent('Location found.');
+	        map.setCenter(pos);
+	      }, function() {
+	        handleLocationError(true, infoWindow, map.getCenter());
+	      });
+	    } else {
+	      // Browser doesn't support Geolocation
+	      handleLocationError(false, infoWindow, map.getCenter());
+	    }
+	}
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-}
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	        infoWindow.setPosition(pos);
+	        infoWindow.setContent(browserHasGeolocation ?
+	                              'Error: The Geolocation service failed.' :
+	                              'Error: Your browser doesn\'t support geolocation.');
+	}
 
-function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -33.8688, lng: 151.2195},
-      zoom: 13,
-      mapTypeId: 'roadmap'
-    });
+	function initAutocomplete() {
+	    var map = new google.maps.Map(document.getElementById('map'), {
+	      center: {lat: -33.8688, lng: 151.2195},
+	      zoom: 13,
+	      mapTypeId: 'roadmap'
+	    });
 
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	    // Create the search box and link it to the UI element.
+	    var input = document.getElementById('pac-input');
+	    var searchBox = new google.maps.places.SearchBox(input);
+	    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
+	    // Bias the SearchBox results towards current map's viewport.
+	    map.addListener('bounds_changed', function() {
+	      searchBox.setBounds(map.getBounds());
+	    });
 
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-      var places = searchBox.getPlaces();
+	    var markers = [];
+	    // Listen for the event fired when the user selects a prediction and retrieve
+	    // more details for that place.
+	    searchBox.addListener('places_changed', function() {
+	      var places = searchBox.getPlaces();
 
-      if (places.length === 0) {
-        return;
-      }
+	      if (places.length === 0) {
+	        return;
+	      }
 
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
+	      // Clear out the old markers.
+	      markers.forEach(function(marker) {
+	        marker.setMap(null);
+	      });
+	      markers = [];
 
-      // For each place, get the icon, name and location.
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
+	      // For each place, get the icon, name and location.
+	      var bounds = new google.maps.LatLngBounds();
+	      places.forEach(function(place) {
+	        if (!place.geometry) {
+	          console.log("Returned place contains no geometry");
+	          return;
+	        }
+	        var icon = {
+	          url: place.icon,
+	          size: new google.maps.Size(71, 71),
+	          origin: new google.maps.Point(0, 0),
+	          anchor: new google.maps.Point(17, 34),
+	          scaledSize: new google.maps.Size(25, 25)
+	        };
 
-        // Create a marker for each place.
-        markers.push(new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
+	        // Create a marker for each place.
+	        markers.push(new google.maps.Marker({
+	          map: map,
+	          icon: icon,
+	          title: place.name,
+	          position: place.geometry.location
+	        }));
 
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
-  }
+	        if (place.geometry.viewport) {
+	          // Only geocodes have viewport.
+	          bounds.union(place.geometry.viewport);
+	        } else {
+	          bounds.extend(place.geometry.location);
+	        }
+	      });
+	      map.fitBounds(bounds);
+	    });
+	  }
 
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-function geolocate() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var geolocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            var circle = new google.maps.Circle({
-                center: geolocation,
-                radius: position.coords.accuracy
-            });
-            autocomplete.setBounds(circle.getBounds());
-        });
-    }
-}
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	function geolocate() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(function(position) {
+	            var geolocation = {
+	                lat: position.coords.latitude,
+	                lng: position.coords.longitude
+	            };
+	            var circle = new google.maps.Circle({
+	                center: geolocation,
+	                radius: position.coords.accuracy
+	            });
+	            autocomplete.setBounds(circle.getBounds());
+	        });
+	    }
+	}
 
-// Adds a marker to the map and push to the array.
-function addMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-    markers.push(marker);
-    console.log(markers);
-}
+	// Adds a marker to the map and push to the array.
+	function addMarker(location) {
+	    var marker = new google.maps.Marker({
+	        position: location,
+	        map: map
+	    });
+	    markers.push(marker);
+	    console.log(markers);
+	}
 
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-    }
-}
+	// Sets the map on all markers in the array.
+	function setMapOnAll(map) {
+	    for (var i = 0; i < markers.length; i++) {
+	        markers[i].setMap(map);
+	    }
+	}
 
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-    setMapOnAll(null);
-}
+	// Removes the markers from the map, but keeps them in the array.
+	function clearMarkers() {
+	    setMapOnAll(null);
+	}
 
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-    clearMarkers();
-    markers = [];
-}
+	// Deletes all markers in the array by removing references to them.
+	function deleteMarkers() {
+	    clearMarkers();
+	    markers = [];
+	}
 
-// Shows any markers currently in the array.
-function showMarkers() {
-    setMapOnAll(map);
-}
+	// Shows any markers currently in the array.
+	function showMarkers() {
+	    setMapOnAll(map);
+	}
 
-function newResults() {
-    console.log(typeof(markerArr));
-    console.log(markerArr);
+	function newResults() {
+	    console.log(typeof(markerArr));
+	    console.log(markerArr);
 
-    //Refreshes the map results div with the new information
-    $("#list2").empty();
+	    //Refreshes the map results div with the new information
+	    $("#list2").empty();
 
-    if (markerArr) {
-        for (var i = 0; i < markerArr.length; i++) {
-            markerArr[i].setMap(null);
-        }
-    }
-    infoWindow = new google.maps.InfoWindow();
-    service = new google.maps.places.PlacesService(map);
+	    if (markerArr) {
+	        for (var i = 0; i < markerArr.length; i++) {
+	            markerArr[i].setMap(null);
+	        }
+	    }
+	    infoWindow = new google.maps.InfoWindow();
+	    service = new google.maps.places.PlacesService(map);
 
-    var type = $(this).text();
-    var request = {
-        bounds: map.getBounds(),
-        map: map,
-        location: haightAshbury,
-        keyword: type,
-        rankBy: google.maps.places.RankBy.PROMINENCE,
-        radius: 5000,
-        zoom: 13,
-        limit: 5,
-    };
+	    var type = $(this).text();
+	    var request = {
+	        bounds: map.getBounds(),
+	        map: map,
+	        location: haightAshbury,
+	        keyword: type,
+	        rankBy: google.maps.places.RankBy.PROMINENCE,
+	        radius: 5000,
+	        zoom: 13,
+	        limit: 5,
+	    };
 
-    service.radarSearch(request, callback);
+	    service.radarSearch(request, callback);
 
-    function callback(results, status) {
-        // console.log(results)//Array of results with place information
-        markerArr = [];
-        for (var i = 0; i < results.length; i++) {
-            service.getDetails(results[i], function(result, status) {
-                // console.log(result);
-                if (result.rating > 4) {
-	                console.log("Only the best of the best, Ratings are greater than 4");
-	                addMarker(result);
-	                addResults(result);
-                }                  
-            });
-        }//End for loop
-    }
+	    function callback(results, status) {
+	        // console.log(results)//Array of results with place information
+	        markerArr = [];
+	        for (var i = 0; i < results.length; i++) {
+	            service.getDetails(results[i], function(result, status) {
+	                // console.log(result);
+	                if (result.rating > 4) {
+		                console.log("Only the best of the best, Ratings are greater than 4");
+		                addMarker(result);
+		                addResults(result);
+	                }                  
+	            });
+	        }//End for loop
+	    }
 
-    function addResults(place) {
-            var b = $('<button>');
-                b.addClass('btn btn-default addToItin');
-                b.text('Add To Itinerary');
-                b.attr('data-name', place.name);
-                b.attr('data-addr', place.formatted_address);
-                b.attr('data-phone', place.formatted_phone_number);
-                b.attr('data-rating', place.rating);
-    
-            $('#list2').append("<li><p><b>Name: </b>" + place.name + "</p><p><b>Address: </b>" + place.formatted_address + "</p><p><b>Phone Number: </b>" + place.formatted_phone_number + "</p><p><b>Rating: </b>" + place.rating + "</p></li>");
-            $('#list2').append(b);
-    }
+	    function addResults(place) {
+	            var b = $('<button>');
+	                b.addClass('btn btn-default addToItin');
+	                b.text('Add To Itinerary');
+	                b.attr('data-name', place.name);
+	                b.attr('data-addr', place.formatted_address);
+	                b.attr('data-phone', place.formatted_phone_number);
+	                b.attr('data-rating', place.rating);
+	    
+	            $('#list2').append("<li><p><b>Name: </b>" + place.name + "</p><p><b>Address: </b>" + place.formatted_address + "</p><p><b>Phone Number: </b>" + place.formatted_phone_number + "</p><p><b>Rating: </b>" + place.rating + "</p></li>");
+	            $('#list2').append(b);
+	    }
 
-    function addMarker(place) {
+	    function addMarker(place) {
 
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location,
+	        var marker = new google.maps.Marker({
+	            map: map,
+	            position: place.geometry.location,
 
-        });
-        markerArr.push(marker);
-        console.log(markerArr);
+	        });
+	        markerArr.push(marker);
+	        console.log(markerArr);
 
-        google.maps.event.addListener(marker, 'click', function() {
-            service.getDetails(place, function(result, status) {
-                // console.log(result);
-                if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                    console.error(status);
-                    return;
-                }
-                infoWindow.setContent("<p><b>Name:</b>" + result.name + "<p><b>address:  </b>" + result.formatted_address + "<p><b>phone number:  </b>" + result.formatted_phone_number + "<p><b>rating: </b>" + result.rating);
-                infoWindow.open(map, marker);
-            });
-        });
-    }
-}
+	        google.maps.event.addListener(marker, 'click', function() {
+	            service.getDetails(place, function(result, status) {
+	                // console.log(result);
+	                if (status !== google.maps.places.PlacesServiceStatus.OK) {
+	                    console.error(status);
+	                    return;
+	                }
+	                infoWindow.setContent("<p><b>Name:</b>" + result.name + "<p><b>address:  </b>" + result.formatted_address + "<p><b>phone number:  </b>" + result.formatted_phone_number + "<p><b>rating: </b>" + result.rating);
+	                infoWindow.open(map, marker);
+	            });
+	        });
+	    }
+	}
 
 	var user = JSON.parse(sessionStorage.getItem('user'));
 
@@ -348,9 +348,6 @@ function newResults() {
 	};
 
 	initMap();
-
-	initAutocomplete();
-
 
 
 	$('.btn').on('click', function(){
